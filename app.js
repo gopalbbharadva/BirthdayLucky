1e7bf3a6d49272da8da31e6cd0c1e4ffc3f10cd4
gopalbharadva <46951254+gopalbharadva@users.noreply.github.com>
2020-12-23 21:45:53 +0530

    Backend start

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 var inputDate=document.querySelector("#input");
+var luckyno=document.querySelector("#luckynumber");
 var button=document.querySelector("#processbtn");
 var outputtext=document.querySelector("#output");
+  
+var sum=0,temp;
 
-button.addEventListener("click",function printDate() {
-    outputtext.textContent=inputDate.value;
+function process(datePart)
+{
+    while(datePart!=0)
+    {
+        temp=datePart%10;
+        sum+=temp;
+        // console.log(temp);
+        datePart/=10;
+    }
+    console.log(sum);
     
-});
+}
+function calculate()
+{
+    var input=inputDate.value;    
+    var date=input.split("-");
+    var year=date[0];
+    var day=date[2];
+    var month=date[1];
+    for(i=0;i<date.length;i++)
+        process(date[i]);
+    
+}
+button.addEventListener("click",calculate);
