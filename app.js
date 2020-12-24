9d2e9f0cc06558c45e665b7a7e61b32e6a759435
gopalbharadva <46951254+gopalbharadva@users.noreply.github.com>
2020-12-24 11:27:11 +0530

    processing is completed

--- app.js
+++ app.js
@@ -3,29 +3,35 @@ var luckyno=document.querySelector("#luckynumber");
 var button=document.querySelector("#processbtn");
 var outputtext=document.querySelector("#output");
   
-var sum=0,temp;
+var t,allsum=0,sum=0,temp,abc=[];
+var islucky=false;
 
 function process(datePart)
 {
-    while(datePart!=0)
+    while(datePart>0)
     {
         temp=datePart%10;
+        // var temp2=temp.toString().split(".")[0];
         sum+=temp;
-        // console.log(temp);
         datePart/=10;
+        var datePart=datePart.toString().split(".")[0];
+        // console.log(datePart);
     }
-    console.log(sum);
-    
 }
 function calculate()
 {
     var input=inputDate.value;    
     var date=input.split("-");
-    var year=date[0];
-    var day=date[2];
-    var month=date[1];
-    for(i=0;i<date.length;i++)
+    for(i=0;i<date.length;i++)  
         process(date[i]);
-    
+
+    t=sum%luckyno.value;
+    console.log(t);
+    if(t==0)
+        islucky=true;
+    else
+        islucky=false;
+    // console.log(sum);
+    outputtext.textContent=islucky;
 }
 button.addEventListener("click",calculate);
